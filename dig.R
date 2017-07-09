f828b2d38f6ff1fb05486b6fe373442c64872fa2
Najeeb Khan <najeebnjk20@yahoo.com>
2017-07-09 22:24:26 +0530

    Predictive model for digit recognizer

---
+++ dig.R
@@ -0,0 +1,14 @@
+library(randomForest)
+library(readr)
+set.seed(0)
+numTrain <- 5000
+numTrees <- 30
+train <- read_csv("../input/train.csv")
+test <- read_csv("../input/test.csv")
+rows <- sample(1:nrow(train), numTrain)
+labels <- as.factor(train[rows,1])
+train <- train[rows,-1]
+rf <- randomForest(train, labels, xtest=test, ntree=numTrees)
+predictions <- data.frame(ImageId=1:nrow(test), Label=levels(labels)[rf$test$predicted])
+head(predictions)
+write_csv(predictions, "rf_benchmark.csv") 
